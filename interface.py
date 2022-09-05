2d20e8ac878bdf22b07781f81d3ccc32fbf8ba31
Patrick <patricketter1@gmail.com>
2022-09-05 12:36:17 -0400

    added while loop

---
+++ interface.py
@@ -0,0 +1,12 @@
+def interface():
+    print('my programs')
+    print('options')
+    print('9-quit')
+    keep_running = True
+    while keep_running:
+        choice = input('enter choice: ')
+        if choice == '9':
+            return
+
+
+interface()
