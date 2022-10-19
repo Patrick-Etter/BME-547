94f31c4a8cc9ded0a2bcae609b3a508b6167e5b1
Patrick <patricketter1@gmail.com>
2022-10-19 13:15:29 -0400

    Learned how to create a server

---
+++ client.py
@@ -0,0 +1,21 @@
+
+
+import requests
+
+# r = requests.get('http://127.0.0.1:5000/info')
+# print(r.status_code)
+# print(r.text)
+'''
+out_data = {'name': 'Patrick Etter',
+            'hdl_value': 15}
+r = requests.post('http://127.0.0.1:5000/hdl_check', json=out_data)
+print(r.status_code)
+print(r.text)
+'''
+
+out_data = {'a': 5, 'b': 12}
+r = requests.post('http://127.0.0.1:5000/add_numbers', json=out_data)
+print(r.status_code)
+print(r.text)
+
+
