be3f250a56ca30c6f810f575d0b8bddc09805637
Patrick <patricketter1@gmail.com>
2022-10-24 12:21:33 -0400

    Learned url math function

--- client.py
+++ client.py
@@ -12,10 +12,13 @@ r = requests.post('http://127.0.0.1:5000/hdl_check', json=out_data)
 print(r.status_code)
 print(r.text)
 '''
-
+'''
 out_data = {'a': 5, 'b': 12}
 r = requests.post('http://127.0.0.1:5000/add_numbers', json=out_data)
 print(r.status_code)
 print(r.text)
 
-
+'''
+r = requests.get('http://127.0.0.1:5000/add/2/7')
+print(r.status_code)
+print(r.text)
